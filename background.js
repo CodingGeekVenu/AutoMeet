be24b8eaef80425a13cfda08ca9884f78e95da03
Venumadhav S <139720141+CodingGeekVenu@users.noreply.github.com>
2025-02-10 00:06:32 +0530

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,14 @@
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("Extension Installed!");
+    console.log("AutoMeet Extension Installed!");
+});
+
+// Handle transcription request from the popup
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "startTranscription") {
+        fetch('http://127.0.0.1:5000/transcribe', { method: "POST" })
+            .then(response => response.json())
+            .then(data => sendResponse({ message: data.message }))
+            .catch(error => sendResponse({ message: "Error: " + error }));
+        return true;
+    }
 });
